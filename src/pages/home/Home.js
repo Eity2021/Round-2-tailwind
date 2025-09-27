2f0afcff95833887fe32c4eb68baee79c994cb79
Eity2021 <Eity2021akter@gmail.com>
2025-09-27 19:09:46 +0600

    steps part design done with responsive

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -3,12 +3,14 @@ import Banner from "./Banner";
 import Exploration from "./Exploration";
 import Faq from "./Faq";
 import Glance from "./Glance";
+import HowItWorksSection from "./HowItWorksSection";
 import Partners from "./Partners";
 
 export default function Home() {
   return (
     <div className="">
       <Partners></Partners>
+      <HowItWorksSection></HowItWorksSection>
       <Glance></Glance>
       <Banner></Banner>
       <Exploration></Exploration>
